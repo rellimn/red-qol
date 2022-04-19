3de671a27c02ed06ff678a01fc3441e52894b293
rellimn <66418892+rellimn@users.noreply.github.com>
2022-04-19 20:26:38 +0200

    Implement Instant Text

--- home/print_text.asm
+++ home/print_text.asm
@@ -2,6 +2,10 @@
 ; screen unless the player presses the A/B button or the delay is turned off
 ; through the [wd730] or [wLetterPrintingDelayFlags] flags.
 PrintLetterDelay::
+	ld a, [wHacks] ; Loads Hacks config into A
+	bit BIT_TEXT_BOX_INSTANT, a ; Sets Z to 1 if bit BIT_TEXT_BOX_INSTANT of A (wHacks) is 0
+	jp nz, .noPrintLetterDelay ; If Z is not set, i. e. Text Box Speed is Instant, continue to .noPrintLetterDelay
+	;
 	ld a, [wd730]
 	bit 6, a
 	ret nz
@@ -42,4 +46,5 @@ PrintLetterDelay::
 	pop bc
 	pop de
 	pop hl
+.noPrintLetterDelay
 	ret
